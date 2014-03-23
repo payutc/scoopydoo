1697f753da2370fffce3dcf87afb3889103c6db2
alecalve <antoine@alc.io>
2014-03-23 12:17:52 +0100

    Oubli de conf.IMPLEMENTED

--- conf.dist.py
+++ conf.dist.py
@@ -7,3 +7,10 @@ SECRET = ''
 HOSTNAME = 'http://localhost:5000'
 PAYUTC_URL = "https://assos.utc.fr/payutc"
 PAYUTC_USER_AGENT = 'scoopydoo'
+
+IMPLEMENTED = {
+    "BLOCAGE": {"name": "Blocage", "url": "blocage.index"},
+    "MESSAGES": {"name": "Messages", "url": "messages.index"},
+    "ADMINRIGHT": {"name": "Droits", "url": "droits.index"},
+    "GESARTICLE": {"name": "Articles", "url": "articles.index"},
+}
